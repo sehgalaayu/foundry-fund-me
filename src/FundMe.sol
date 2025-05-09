8a1db9b7c143b22d2e18afc111321c1bc48d6710
sehgalaayu <119155282+sehgalaayu@users.noreply.github.com>
2025-05-09 23:21:14 +0530

    Update FundMe.sol

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MI
 pragma solidity ^0.8.18;
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
