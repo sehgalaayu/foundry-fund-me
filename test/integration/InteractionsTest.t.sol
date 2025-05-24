c5ef060f9d71fa6f16f81e4ce5b111de54be09ab
sehgalaayu <119155282+sehgalaayu@users.noreply.github.com>
2025-05-24 23:54:48 +0530

    Update InteractionsTest.t.sol

--- test/integration/InteractionsTest.t.sol
+++ test/integration/InteractionsTest.t.sol
@@ -5,14 +5,13 @@ pragma solidity 0.8.19;
 import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
 import {FundFundMe, WithdrawFundMe} from "../../script/Interactions.s.sol";
 import {FundMe} from "../../src/FundMe.sol";
-// import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import {Test, console} from "forge-std/Test.sol";
 import {StdCheats} from "forge-std/StdCheats.sol";
-// import {ZkSyncChainChecker} from "lib/foundry-devops/src/ZkSyncChainChecker.sol";
+
 
 contract InteractionsTest is StdCheats, Test {
     FundMe public fundMe;
-    // HelperConfig public helperConfig;
+
 
     uint256 public constant SEND_VALUE = 0.1 ether; // just a value to make sure we are sending enough!
     uint256 public constant STARTING_USER_BALANCE = 10 ether;
